88d23d69342b60bd0b5a28b3329955705dabab84
ahmed3mran <a7mad3mran478@gmail.com>
2024-02-17 16:08:54 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let btnShow = document.getElementById('btn-show')
+let btnHide = document.getElementById('btn-hide')
+let toggle = document.getElementById('toggle')
+
+
+btnShow.addEventListener('click', function(){
+  toggle.classList.toggle('show')
+})
+btnHide.addEventListener('click', function(){
+  toggle.classList.toggle('show')
+})
